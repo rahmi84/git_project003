e18201dd81045d929f7790f49008fa173a3be63c
rahmi84 <69377204+rahmi84@users.noreply.github.com>
2020-08-09 15:59:03 -0300

    Update Feature.java
    we ll try to cause some conflict

--- git_project002/Feature.java
+++ git_project002/Feature.java
@@ -8,6 +8,7 @@ System.out.println("this is other class");
 		System.out.println("first commit from Rahmi branch");
 		
 		System.out.println("the second commit from rahmi,2. one");
+		System.out.println("the trial commit for conflict");
 		
 	}
 
