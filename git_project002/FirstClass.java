8e9f6566976bff26350f9ea9b4a1e4b81254828b
Rahmi <magicra84@gmail.com>
2020-08-09 12:46:57 -0300

    here we go

---
+++ git_project002/FirstClass.java
@@ -0,0 +1,14 @@
+package git_project002;
+
+public class FirstClass {
+
+	public static void main(String[] args) {
+System.out.println("This is the first changes from local master");
+System.out.println("this is the second commit from local master");
+System.out.println("this is third commit");
+		System.out.println("This is the first changes from remote repostery");
+		
+		
+	}
+
+}
