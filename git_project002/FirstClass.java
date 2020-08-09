20d04071fdad6c3923573da78da1a553f4491fc0
Rahmi <magicra84@gmail.com>
2020-08-09 12:48:28 -0300

    gere we go

--- git_project002/FirstClass.java
+++ git_project002/FirstClass.java
@@ -7,6 +7,7 @@ System.out.println("This is the first changes from local master");
 System.out.println("this is the second commit from local master");
 System.out.println("this is third commit");
 		System.out.println("This is the first changes from remote repostery");
+		System.out.println("this is the 4. commit from local");
 		
 		
 	}
